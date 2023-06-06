757a2707d2f4650acee281fd71d9d038f1f60e0c
user <marinapanasyk72@mail.ru>
2023-06-06 12:16:45 +0300

    Семинар

---
+++ Задача27.py
@@ -0,0 +1,14 @@
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+
+str = """She sells sea shells on the sea shore The shells
+that she sells are sea shells I'm sure.So if she sells sea
+shells on the sea shore I'm sure that the shells are sea
+shore shells"""
+str = str.lower().replace("."," ").replace("\n"," ")
+spl_str = set(str.split())
+print(spl_str)
+print(len(spl_str))
